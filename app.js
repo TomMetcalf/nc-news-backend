12274e4409c588408786e9fa434b51022e1223e8
Tom Metcalf <intermet@gmail.com>
2023-05-12 10:03:25 +0100

    7_POST_/api/articles/article_id/comments

--- app.js
+++ app.js
@@ -6,23 +6,40 @@ const {
     getArticles,
     getCommentsByArticleId,
 } = require('./controllers/articles.controllers');
+const { postComment } = require('./controllers/comment.controllers');
 const app = express();
+
+app.use(express.json());
 
 app.get('/api/topics', getTopics);
 app.get('/api', getStatus);
 app.get('/api/articles/:article_id', getArticleById);
 app.get('/api/articles', getArticles);
 app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
+app.post('/api/articles/:article_id/comments', postComment);
 
 app.use((err, req, res, next) => {
     if (err.code === '22P02') {
-        res.status(400).send({ msg: 'Bad Request' });
+        res.status(400).send({ msg: 'bad request!' });
     } else next(err);
 });
 
 app.use((err, req, res, next) => {
-    res.status(err.status).send({ msg: err.msg });
-    next(err);
+    if (err.code === '25303') {
+        res.status(400).send({ msg: 'user does not exist!' });
+    } else next(err);
+});
+
+app.use((err, req, res, next) => {
+    if (err.status && err.msg) {
+        res.status(err.status).send({ msg: err.msg });
+    } else next(err);
+});
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    console.log(err.code);
+    res.status(500).send({ msg: 'Internal Server Error!' });
 });
 
 app.all('*', (req, res) => {
